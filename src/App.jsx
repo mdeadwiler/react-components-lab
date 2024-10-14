f5293ce146ef52f1cacb53f50d4e612bed7a5d43
MDeadwiler <mdeadwiler@email.com>
2024-10-14 15:35:38 -0400

    react-components-lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
+import WeatherForecasts from './WeatherForecast';
 import './App.css'
-
-// src/App.jsx
+//import './index.css'
+import './WeatherForecast.css'
 
 const App = () => {
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+    },
+    {
+      day: 'Tue',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+    },
+    {
+      day: 'Wed',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+    },
+    {
+      day: 'Thu',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+    },
+    {
+      day: 'Fri',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+    },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>This is my first Weather App, Cool!</h1>
+      <h1>Local Weather is not real</h1>
+      <section>
+        <WeatherForecasts forecasts={weatherForecasts} />
+      </section>
+    </>
   );
 }
 
-export default App
-
-
-
+export default App;
